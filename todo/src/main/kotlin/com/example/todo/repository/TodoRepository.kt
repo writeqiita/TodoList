b14d1ee04f7a509516a3fe6f3d0a4e34b21d06e9
r_imanishi <r_imanishi@active.co.jp>
2025-06-28 23:45:12 +0900

    todoディレクトリを削除

--- todo/src/main/kotlin/com/example/todo/repository/TodoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.todo.repository
-
-import com.example.todo.model.Todo
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TodoRepository : JpaRepository<Todo, Long>
