eef783e3f3f3458a92909e4f93e43295922ba790
r_imanishi <r_imanishi@active.co.jp>
2025-06-29 13:56:11 +0900

    CORS設定をより細かく設定

--- todo-backend/src/main/kotlin/com/example/todo/config/WebConfig.kt
+++ todo-backend/src/main/kotlin/com/example/todo/config/WebConfig.kt
@@ -7,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+        // バックエンド側の全てのURLに対して、CORS（クロスオリジンリクエスト）設定を適用する
         registry.addMapping("/**")
+            // フロントエンドのURL
             .allowedOrigins("http://localhost:3000")
-            .allowedMethods("*")
+            //　使用するHTTPメソッド
+            .allowedMethods("GET","POST","PUT","DELETE","PATCH") 
     }
 }
